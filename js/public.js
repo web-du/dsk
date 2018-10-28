76b328c5740d7f871f5f18385bbdb1d9e27e9afd
web-du <1605993932@qq.com>
2018-10-28 22:04:15 +0800

    新增至环保页

--- js/public.js
+++ js/public.js
@@ -1,3 +1,14 @@
 $(function(){
-
+    //返回顶部
+    $(window).scroll(function () {
+        if($(window).scrollTop()>=500) {
+            $(".toTop").addClass("now");
+        }else {
+            $(".toTop").removeClass("now");
+        }
+    });
+    $(".toTop").click(function () { 
+        $("html, body").animate({scrollTop: 0}, 300); /* 持续时间为 100ms */
+        return false;
+    });
 })
