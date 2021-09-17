245ab5f93119a6966b151a1c7380c4a3a4c8a57e
duwenzheng <1605993932@qq.com>
2021-09-17 17:55:45 +0800

    feat：新增缓存策略

--- prod.server.js
+++ prod.server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const port = 9001;
 const app = express();
-app.use(express.static('./'));
+app.use(express.static(path.join(__dirname, './'), { maxAge: 86400 * 1000 }));
 module.exports = app.listen(port, function (err) {
     if (err) {
         console.log(err)
