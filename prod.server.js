413d4333a9645780862a9be3b05b07916785b20d
duwenzheng <1605993932@qq.com>
2021-09-22 18:13:43 +0800

    feat：设置浏览器离线缓存文件

--- prod.server.js
+++ prod.server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = 9001;
 const app = express();
-app.use(express.static(path.join(__dirname, './'), { maxAge: 86400 * 1000 }));
+app.use(express.static(path.join(__dirname, './')));
 module.exports = app.listen(port, function (err) {
     if (err) {
         console.log(err)
